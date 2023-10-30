7bfb027dab5b28c858f445562c136deefaf1cc45
yoonjong.park <yoonjongpark@gmail.com>
2023-10-30 21:56:23 +0900

    ADD KakaoMap using Next Script

---
+++ src/components/Map.tsx
@@ -0,0 +1,34 @@
+/* global kakao */
+
+import Script from "next/script";
+
+// declare global : 모듈 파일에서도 전역 참조가 가능한 선언 코드를 작성하고 싶을 때 사용
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+export default function Map() {
+  const loadKakaoMap = () => {
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById("map");
+      const mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      new window.kakao.maps.Map(mapContainer, mapOption);
+    });
+  };
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onReady={loadKakaoMap}
+      />
+      <div id="map" className="w-full h-screen"></div>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,5 @@
-import Link from "next/link";
+import Map from "@/components/Map";
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="font-bold">지도 페이지</h1>
-      <ul>
-        <li>
-          <Link href="/stores">맛집 목록</Link>
-        </li>
-        <li>
-          <Link href="/stores/new">맛집 생성</Link>
-        </li>
-        <li>
-          <Link href="/stores/1">맛집 상세 페이지</Link>
-        </li>
-        <li>
-          <Link href="/stores/1/edit">맛집 수정 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/login">로그인 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/mypage">마이 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/likes">찜한 맛집</Link>
-        </li>
-      </ul>
-    </>
-  );
+  return <Map />;
 }
