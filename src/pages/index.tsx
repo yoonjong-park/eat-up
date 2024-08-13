f78943e296b287e7e3abb470c260649dcefe30ba
yoonjong.park <yoonjongpark@gmail.com>
2024-08-13 14:17:21 +0900

    CHANGE NaverMap -> KakaoMap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-// import KakaoMap from "@/components/KakaoMap";
-import NaverMap from "@/components/NaverMap";
+import KakaoMap from "@/components/KakaoMap";
+// import NaverMap from "@/components/NaverMap";
 
 export default function Home() {
-  return <NaverMap />;
+  return <KakaoMap />;
 }
