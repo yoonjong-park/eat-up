d9605ef5db508706c086e807d91deff9f4fb7c81
yoonjong.park <yoonjongpark@gmail.com>
2024-08-16 15:55:11 +0900

    ADD changing render marker Image

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -32,6 +32,21 @@ export default function KakaoMap() {
 
       // render MARKER
       stores?.["DATA"]?.map((store) => {
+        // 마커 설정
+        const imageSrc = store?.bizcnd_code_nm
+          ? `/images/markers/${store.bizcnd_code_nm}.png`
+          : "/images/markers/default.png";
+
+        const imageSize = new kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
+        const imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+        // 마커이미지를 생성
+        const markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption
+        );
+
         // 마커 위치 설정
         const markerPosition = new window.kakao.maps.LatLng(
           store?.y_dnts,
@@ -41,6 +56,7 @@ export default function KakaoMap() {
         // 마커 생성
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
+          image: markerImage,
         });
 
         // 마커를 지도 위에 설정
